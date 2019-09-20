df007f324873bc3bc69373af46933eac0eb6f9b2
Tobias Gubo <tobias.gubo@continental-corporation.com>
2019-09-20 15:41:22 +0200

    Added some ios code

---
+++ ios/Classes/PermissionEnums.h
@@ -0,0 +1,42 @@
+//
+//  PermissionEnums.h
+//  Pods
+//
+//  Created by Tobias Gubo on 20.09.19.
+//
+
+typedef NS_ENUM(int, PermissionGroup) {
+    PermissionGroupCalendar = 0,
+    PermissionGroupCamera,
+    PermissionGroupContacts,
+    PermissionGroupLocation,
+    PermissionGroupLocationAlways,
+    PermissionGroupLocationWhenInUse,
+    PermissionGroupMediaLibrary,
+    PermissionGroupMicrophone,
+    PermissionGroupPhone,
+    PermissionGroupPhotos,
+    PermissionGroupReminders,
+    PermissionGroupSensors,
+    PermissionGroupSms,
+    PermissionGroupSpeech,
+    PermissionGroupStorage,
+    PermissionGroupIgnoreBatteryOptimizations,
+    PermissionGroupUnknown,
+};
+
+typedef NS_ENUM(int, PermissionStatus) {
+    PermissionStatusDenied = 0,
+    PermissionStatusDisabled,
+    PermissionStatusGranted,
+    PermissionStatusRestricted,
+    PermissionStatusUnknown,
+};
+
+typedef NS_ENUM(int, ServiceStatus) {
+    ServiceStatusDisabled = 0,
+    ServiceStatusEnabled,
+    ServiceStatusNotApplicable,
+    ServiceStatusUnknown,
+};
+
